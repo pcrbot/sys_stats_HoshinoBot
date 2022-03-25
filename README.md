1de3ac987da493f57012560b20e1a5600d052a47
SlightDust <zzc_200014@163.com>
2022-03-25 22:58:23 +0800

    初次上传，摸了

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # sys_stats_HoshinoBot
-检查bot服务器的硬件信息（适用于Linux和Windows）
+检查bot服务器的硬件信息（适用于Linux和Windows）  
+
+没什么技术含量，都是直接调库读取的，在linux下也可以直接读文件，但我搞不太来，写好发我（）
+
+之前看到了[pcrbot/advance_check_hoshinobot](https://github.com/pcrbot/advance_check_hoshinobot)，想用一下，发现[SYoung](https://github.com/Soung2279)大佬是用wmi库写的，这个库在linux下不太好使，就摸了一个linux版的（其实win也能用，我是现在win写好再去服务器加linux功能的）
+
+需要以下库
+```
+psutil
+distro
+```
+手动pip install吧，懒得弄requirements了。
+
+# 安装和使用方法
+1. 在hoshino/modules下clone本仓库`git clone https://github.com/pcrbot/sys_stats_HoshinoBot.git`  
+2. 在hoshino/config/\_\_bot\_\_.py中加入
+```
+MODULES_ON = {
+...
+'sys_stats_HoshinoBot',  # 硬件信息
+}
+```
+3. 重启hoshino
+
+在群内发送`@bot check` `@bot 自检` `@bot 鲁大师`即可。
+
+# 效果
+bot会发送如下内容：  
+```
+System Info:
+平台:
+ 运行平台: CentOS Linux 7.9.2009
+ 机器类型: x86_64
+cpu:
+ 内核数: 2
+ 逻辑CPU: 2
+ 基准速度: 2.60 GHz
+ 利用率: 4.9 (在0.2秒内)
+物理内存:
+ 总物理内存: 3.7G
+ 已使用: 2.2G (67.8%)
+ 剩余: 1.2G
+swap
+ swap总大小: 1024.0M
+ 已使用: 128.5M (12.5%)
+ 剩余: 895.5M
+ 累计I/O: 25.1M / 175.4M
+硬盘:
+ 标识: /dev/vda1 挂载点: / 总空间: 58.9G 已使用: 35.1G (62.2%) 剩余: 21.3G
+```
---
+++ sys_stats_HoshinoBot.py
@@ -0,0 +1,136 @@
+import psutil
+import platform
+import hoshino
+from hoshino import Service, priv
+from hoshino.util import FreqLimiter
+
+islinux = platform.system().lower() == "linux"
+
+if islinux:
+    import distro
+
+
+flmt = FreqLimiter(30)
+
+sv = Service(
+    name="系统自检",  # 功能名
+    use_priv=priv.NORMAL,  # 使用权限
+    manage_priv=priv.ADMIN,  # 管理权限
+    visible=True,  # False隐藏
+    enable_on_default=True,  # 是否默认启用
+)
+
+
+def human(n):
+    symbols = ("K", "M", "G", "T", "P", "E", "Z", "Y")
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return "%.1f%s" % (value, s)
+    return "%sB" % n
+
+
+def get_platform():
+    platform_info = {
+        "运行平台": f"{platform.system()} {platform.version()}",
+        "机器类型": platform.machine(),  # 这个该叫什么？
+    }
+    if islinux:
+        platform_info.update(
+            {
+                "运行平台": f"{distro.distro_release_info()['name']} {distro.distro_release_info()['version_id']}",
+            }
+        )
+    return platform_info
+
+
+def get_cpu():
+    cpu_info = {
+        # "CPU": platform.processor(),
+        "内核数": psutil.cpu_count(logical=False),
+        "逻辑CPU": psutil.cpu_count(),
+        "基准速度": f"{psutil.cpu_freq()[0]/1000:.2f} GHz",
+        "利用率": f"{psutil.cpu_percent(interval=0.2)} (在0.2秒内)",
+    }
+    return cpu_info
+
+
+def get_memory():
+    virmem = psutil.virtual_memory()
+    memory_info = {
+        "总物理内存": human(virmem[0]),
+        "已使用": f"{human(virmem[3])} ({virmem[2]}%)",
+        "剩余": human(virmem[1]),
+    }
+    return memory_info
+
+
+def get_swap():
+    swapmem = psutil.swap_memory()
+    swap_info = {
+        f"{'虚拟内存' if platform.system().lower()=='windows' else 'swap'}总大小": human(
+            swapmem[0]
+        ),
+        "已使用": f"{human(swapmem[1])} ({swapmem[3]}%)",
+        "剩余": human(swapmem[2]),
+        "累计I/O": f"{human(swapmem[4])} / {human(swapmem[5])}",
+    }
+    return swap_info
+
+
+def get_disk():
+    disk_list = []
+    disk_info = []
+    for disk in psutil.disk_partitions():
+        disk_list.append([disk[0], disk[1]])  # 标识，挂载点
+    for disk in disk_list:
+        usage = psutil.disk_usage(disk[1])
+        disk_temp = {
+            "标识": disk[0],
+        }
+        if islinux:
+            disk_temp.update({"挂载点": disk[1]})
+        disk_temp.update(
+            {
+                "总空间": human(usage[0]),
+                "已使用": f"{human(usage[1])} ({usage[3]}%)",
+                "剩余": human(usage[2]),
+            }
+        )
+        disk_info.append(disk_temp)
+    return disk_info
+
+
+@sv.on_fullmatch(("check","自检","鲁大师"),only_to_me=True)
+async def sys_stats(bot,ev):
+    # 冷却器检查，每群30秒CD
+    if not flmt.check(ev['group_id']):
+        await bot.send(ev, f"自检冷却中，请在{flmt.left_time(ev['group_id']):.2f}秒后再试~")
+        return
+    flmt.start_cd(ev['group_id'])
+    await bot.send(ev,"正在执行自检……")
+    msg = "System Info:\n"
+    msg += "平台:\n"
+    for key, value in get_platform().items():
+        msg += f" {key}: {value}\n"
+    msg += "cpu:\n"
+    for key, value in get_cpu().items():
+        msg += f" {key}: {value}\n"
+    msg += "物理内存:\n"
+    for key, value in get_memory().items():
+        msg += f" {key}: {value}\n"
+    msg += ("虚拟内存" if platform.system().lower() == "windows" else "swap") + "\n"
+    for key, value in get_swap().items():
+        msg += f" {key}: {value}\n"
+    msg += "硬盘:\n"
+    for disks in get_disk():
+        for key, value in disks.items():
+            msg += f" {key}: {value}"
+        msg += "\n"
+    try:
+        await bot.send(ev,msg)
+    except:
+        await bot.finish(ev,"自检结果发送失败！")
